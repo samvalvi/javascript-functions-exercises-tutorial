f59db9ab61681d557b1b23451ee54f6e816cca0a
Samuel <samuelvalerin@gmail.com>
2021-02-18 12:49:26 -0600

    first commit

--- exercises/04-Call-a-function/app.js
+++ exercises/04-Call-a-function/app.js
@@ -5,7 +5,12 @@ function calculateArea(length,edge)
 // Your code Below this line:
 
 
+let squareArea1 = calculateArea(3,6);
+let squareArea2 = calculateArea(6,8);
+let squareArea3 = calculateArea(4,10); 
 
-
+console.log(squareArea1);
+console.log(squareArea2);
+console.log(squareArea3);
 
 
